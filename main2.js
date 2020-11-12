c6c8ad82b3c77cbb555b40e6469c261802c411d2
gbishop16 <gqbishop@gmail.com>
2020-11-12 09:45:17 -0800

    nov 12 class
    css file, js file, editing html file as well, click event, unloading data

---
+++ main2.js
@@ -0,0 +1,12 @@
+function unloadContent(){
+  $("#main_container").html('');
+}
+$("#main_container").removeClass("start");
+
+funcation loadContent(){
+  unloadContent();
+  $(#main_container).addClass("heading")
+}
+document.getElementById("rick_roll").oneclick = function (){
+  location.href = "https://www.youtube.com/watch?v=YddwkMJG1Jo";
+}
