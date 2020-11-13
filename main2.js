37b4bc5055342c6935ced5c6646a776900c1430a
gbishop16 <gqbishop@gmail.com>
2020-11-13 10:42:10 -0800

    Button for rick roll

--- main2.js
+++ main2.js
@@ -3,10 +3,10 @@ function unloadContent(){
 }
 $("#main_container").removeClass("start");
 
-funcation loadContent(){
+function loadContent(){
   unloadContent();
-  $(#main_container).addClass("heading")
+  $("#main_container").addClass("heading")
 }
-document.getElementById("rick_roll").oneclick = function (){
+document.getElementById("rick_roll").onclick = function (){
   location.href = "https://www.youtube.com/watch?v=YddwkMJG1Jo";
 }
