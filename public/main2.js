fbbe64e1c8ae8c423dcf82977ffc7cd1f8183585
gbishop16 <gqbishop@gmail.com>
2020-11-18 10:10:47 -0800

    tech spec link

--- public/main2.js
+++ public/main2.js
@@ -10,14 +10,12 @@ function loadContent(){
 function loadGameContent(result){
   $('#game_content').html(result);
 }
-if(document.getElementById("open_game"))
-{
+if(document.getElementById("open_game")){
 document.getElementById("open_game").onclick = function (){
   $.ajax({url: "game.html", success: loadGameContent});
 }
 }
-if(document.getElementById("open_game"))
-{
+if(document.getElementById("rick_roll")){
 document.getElementById("rick_roll").onclick = function (){
   location.href = "https://www.youtube.com/watch?v=YddwkMJG1Jo";
 }
@@ -34,3 +32,9 @@ document.getElementById("about_me").onclick = function (){
 document.getElementById("home_page").onclick = function (){
   location.href = "index.html";
 }
+if(document.getElementById("tech_spec"))
+{
+document.getElementById("tech_spec").onclick = function (){
+  location.href = "https://docs.google.com/document/d/11KRt5S18rvGJxmNQHIDBENf6uVg6nR6VkC93a2m3XCg/edit";
+}
+};
