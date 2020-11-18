efaf36e745cc0069b633593381b37c2bb3919200
gbishop16 <gqbishop@gmail.com>
2020-11-17 18:35:10 -0800

    Game window
    game window and fixing tech spec

--- public/main2.js
+++ public/main2.js
@@ -7,12 +7,24 @@ function loadContent(){
   unloadContent();
   $("#main_container").addClass("heading")
 }
+function loadGameContent(result){
+  $('#game_content').html(result);
+}
+document.getElementById("open_game").onclick = function (){
+  $.ajax({url: "game.html", success: loadGameContent});
+}
 document.getElementById("rick_roll").onclick = function (){
   location.href = "https://www.youtube.com/watch?v=YddwkMJG1Jo";
 }
 document.getElementById("game_page").onclick = function (){
-  location.href = "page2.html";
+  location.href = "game.html";
 }
 document.getElementById("github_link").onclick = function (){
   location.href = "https://github.com/gbishop16/MyWebsite";
+}
+document.getElementById("about_me").onclick = function (){
+  location.href = "aboutme.html";
+}
+document.getElementById("home_page").onclick = function (){
+  location.href = "index.html";
 }
